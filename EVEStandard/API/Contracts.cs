6f04108fa8667b015852a7460fc5d8ba7d2e6d45
Gehnster <gehnster@withineveonline>
2018-04-03 21:01:41 -0600

    Contracts end-points

---
+++ EVEStandard/API/Contracts.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVEStandard.API
+{
+    using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
+    using System.Threading.Tasks;
+    using Enumerations;
+    using Models;
+    using Models.API;
+
+    public class Contracts : APIBase
+    {
+        private ILogger Logger { get; } = LibraryLogging.CreateLogger<Contracts>();
+        internal Contracts(string dataSource) : base(dataSource)
+        {
+        }
+
+        public async Task<(List<Contract>, long)> GetContractsV1Async(AuthDTO auth, long page)
+        {
+            checkAuth(auth, Scopes.ESI_CONTRACTS_READ_CHARACTER_CONTRACTS_1);
+
+            var queryParameters = new Dictionary<string, string>
+            {
+                { "page", page.ToString() }
+            };
+
+            var responseModel = await this.GetAsync("/v1/characters/" + auth.Character.CharacterID + "/contracts/", auth, queryParameters);
+
+            checkResponse("GetContractsV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return (JsonConvert.DeserializeObject<List<Contract>>(responseModel.JSONString), responseModel.MaxPages);
+        }
+
+        public async Task<List<ContractItem>> GetContractItemsV1Async(AuthDTO auth, long contractId)
+        {
+            checkAuth(auth, Scopes.ESI_CONTRACTS_READ_CHARACTER_CONTRACTS_1);
+
+            var responseModel = await this.GetAsync("/v1/characters/" + auth.Character.CharacterID + "/contracts/" + contractId + "/items/", auth);
+
+            checkResponse("GetContractItemsV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<List<ContractItem>>(responseModel.JSONString);
+        }
+
+        public async Task<List<ContractBid>> GetContractBidsV1Async(AuthDTO auth, long contractId)
+        {
+            checkAuth(auth, Scopes.ESI_CONTRACTS_READ_CHARACTER_CONTRACTS_1);
+
+            var responseModel = await this.GetAsync("/v1/characters/" + auth.Character.CharacterID + "/contracts/" + contractId + "/bids/", auth);
+
+            checkResponse("GetContractBidsV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<List<ContractBid>>(responseModel.JSONString);
+        }
+
+        public async Task<(List<Contract>, long)> GetCorporationContractsV1Async(AuthDTO auth, long page, long corporationId)
+        {
+            checkAuth(auth, Scopes.ESI_CONTRACTS_READ_CORPORATION_CONTRACTS_1);
+
+            var queryParameters = new Dictionary<string, string>
+            {
+                { "page", page.ToString() }
+            };
+
+            var responseModel = await this.GetAsync("/v1/corporations/" + corporationId + "/contracts/", auth, queryParameters);
+
+            checkResponse("GetCorporationContractsV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return (JsonConvert.DeserializeObject<List<Contract>>(responseModel.JSONString), responseModel.MaxPages);
+        }
+
+        public async Task<List<ContractItem>> GetCorporationContractItemsV1Async(AuthDTO auth, long contractId)
+        {
+            checkAuth(auth, Scopes.ESI_CONTRACTS_READ_CORPORATION_CONTRACTS_1);
+
+            var responseModel = await this.GetAsync("/v1/corporations/" + auth.Character.CharacterID + "/contracts/" + contractId + "/items/", auth);
+
+            checkResponse("GetCorporationContractItemsV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<List<ContractItem>>(responseModel.JSONString);
+        }
+
+        public async Task<List<ContractBid>> GetCorporationContractBidsV1Async(AuthDTO auth, long contractId)
+        {
+            checkAuth(auth, Scopes.ESI_CONTRACTS_READ_CORPORATION_CONTRACTS_1);
+
+            var responseModel = await this.GetAsync("/v1/corporations/" + auth.Character.CharacterID + "/contracts/" + contractId + "/bids/", auth);
+
+            checkResponse("GetCorporationContractBidsV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<List<ContractBid>>(responseModel.JSONString);
+        }
+    }
+}
---
+++ EVEStandard/Models/ContractBid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVEStandard.Models
+{
+    using Newtonsoft.Json;
+
+    public class ContractBid : ModelBase<ContractBid>
+    {
+        /// <summary>
+        /// Unique ID for the bid
+        /// </summary>
+        /// <value>Unique ID for the bid</value>
+        [JsonProperty("bid_id")]
+        public int? BidId { get; set; }
+
+        /// <summary>
+        /// Character ID of the bidder
+        /// </summary>
+        /// <value>Character ID of the bidder</value>
+        [JsonProperty("bidder_id")]
+        public int? BidderId { get; set; }
+
+        /// <summary>
+        /// Datetime when the bid was placed
+        /// </summary>
+        /// <value>Datetime when the bid was placed</value>
+        [JsonProperty("date_bid")]
+        public DateTime? DateBid { get; set; }
+
+        /// <summary>
+        /// The amount bid, in ISK
+        /// </summary>
+        /// <value>The amount bid, in ISK</value>
+        [JsonProperty("amount")]
+        public float? Amount { get; set; }
+    }
+}
