9f0ed8bb4db693aee0c46f393cdd8e893d46f63e
Richard Mountain <web@richardmountain.me>
2018-04-10 14:04:44 +0100

    Created an asteroid model class

---
+++ EVEStandard/Models/AsteroidBelt.cs
@@ -0,0 +1,12 @@
+﻿using EVEStandard.Models;
+using Newtonsoft.Json;
+
+namespace EveCorpMonNet.Libraries.EVEStandard.Models
+{
+    public class AsteroidBelt : ModelBase<AsteroidBelt>
+    {
+        [JsonProperty("name")] public string Name { get; set; }
+        [JsonProperty("position")] public Position Position { get; set; }
+        [JsonProperty("system_id")] public int SystemId { get; set; }
+    }
+}
