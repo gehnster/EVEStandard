c7f7ef45f351c8347af809604c4722be93550248
Richard Mountain <web@richardmountain.me>
2018-04-10 13:35:21 +0100

    Started work on Universe API class, able to get all ancesteries now.

--- EVEStandard/API/Universe.cs
+++ EVEStandard/API/Universe.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using EveCorpMonNet.Libraries.EVEStandard.Models;
+using Newtonsoft.Json;
 
 namespace EVEStandard.API
 {
@@ -10,6 +13,19 @@ namespace EVEStandard.API
         private ILogger Logger { get; } = LibraryLogging.CreateLogger<Universe>();
         internal Universe(string dataSource) : base(dataSource)
         {
+        }
+
+        /// <summary>
+        /// Get all character ancestries
+        /// </summary>
+        /// <returns>List of character ancestries</returns>
+        public async Task<List<Ancestry>> GetAncestriesV1Async()
+        {
+            var responseModel = await this.GetAsync("/v1/universe/ancestries/", null);
+
+            checkResponse("GetAncestriesV1Async", responseModel.Error,responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<List<Ancestry>>(responseModel.JSONString);
         }
     }
 }
