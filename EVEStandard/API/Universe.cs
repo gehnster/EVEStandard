0b6aa32a6b10a6089a6d12f735e17e2360b86b3a
Richard Mountain <web@richardmountain.me>
2018-04-10 14:05:37 +0100

    Added method to get asteroid belt information based on supplied id and method to retrieve all bloodline details

--- EVEStandard/API/Universe.cs
+++ EVEStandard/API/Universe.cs
@@ -21,11 +21,29 @@ namespace EVEStandard.API
         /// <returns>List of character ancestries</returns>
         public async Task<List<Ancestry>> GetAncestriesV1Async()
         {
-            var responseModel = await this.GetAsync("/v1/universe/ancestries/", null);
+            var responseModel = await this.GetAsync("/v1/universe/ancestries/");
 
             checkResponse("GetAncestriesV1Async", responseModel.Error,responseModel.LegacyWarning, this.Logger);
 
             return JsonConvert.DeserializeObject<List<Ancestry>>(responseModel.JSONString);
+        }
+
+        public async Task<AsteroidBelt> GetAsteroidBeltV1Async(int asteroidId)
+        {
+            var responseModel = await this.GetAsync(string.Format("/v1/universe/asteroid_belts/{0}/", asteroidId));
+
+            checkResponse("GetAsteroidBeltV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<AsteroidBelt>(responseModel.JSONString);
+        }
+
+        public async Task<List<Bloodline>> GetBloodlinesV1Async()
+        {
+            var responseModel = await this.GetAsync("/v1/universe/bloodlines/");
+
+            checkResponse("GetBloodlinesV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<List<Bloodline>>(responseModel.JSONString);
         }
     }
 }
