908412959ca798961469cc2b63655cf06fb9be84
Gehnster <gehnster@withineveonline>
2018-04-12 23:36:01 -0600

    Fittings end-point. Not sure if create will work so requires testing

--- EVEStandard/API/Fittings.cs
+++ EVEStandard/API/Fittings.cs
@@ -5,11 +5,48 @@ using System.Text;
 
 namespace EVEStandard.API
 {
+    using System.Threading.Tasks;
+    using Enumerations;
+    using Models;
+    using Models.API;
+    using Newtonsoft.Json;
+
     public class Fittings : APIBase
     {
         private ILogger Logger { get; } = LibraryLogging.CreateLogger<Fittings>();
         internal Fittings(string dataSource) : base(dataSource)
         {
+        }
+
+        public async Task DeleteFittingV1Async(AuthDTO auth, long fittingId)
+        {
+            checkAuth(auth, Scopes.ESI_FITTINGS_WRITE_FITTINGS_1);
+
+            var responseModel = await this.DeleteAsync("/v1/characters/" + auth.Character.CharacterID + "/fittings/" + fittingId + "/", auth);
+
+            checkResponse("DeleteFittingV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+        }
+
+        public async Task<List<CharacterFitting>> GetFittingsV1Async(AuthDTO auth)
+        {
+            checkAuth(auth, Scopes.ESI_FITTINGS_READ_FITTINGS_1);
+
+            var responseModel = await this.GetAsync("/v1/characters/" + auth.Character.CharacterID + "/fittings/", auth);
+
+            checkResponse("TrackCorporationMembersV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<List<CharacterFitting>>(responseModel.JSONString);
+        }
+
+        public async Task<long> CreateFittingV1Async(AuthDTO auth, ShipFitting fitting)
+        {
+            checkAuth(auth, Scopes.ESI_FITTINGS_READ_FITTINGS_1);
+
+            var responseModel = await this.PostAsync("/v1/characters/" + auth.Character.CharacterID + "/fittings/", auth, fitting);
+
+            checkResponse("TrackCorporationMembersV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<long>(responseModel.JSONString);
         }
     }
 }
---
+++ EVEStandard/Models/ShipFitting.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVEStandard.Models
+{
+    using Newtonsoft.Json;
+
+    public class ShipFitting : ModelBase<ShipFitting>
+    {
+        /// <summary>
+        /// name string
+        /// </summary>
+        /// <value>name string</value>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// description string
+        /// </summary>
+        /// <value>description string</value>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// ship_type_id integer
+        /// </summary>
+        /// <value>ship_type_id integer</value>
+        [JsonProperty("ship_type_id")]
+        public int? ShipTypeId { get; set; }
+
+        /// <summary>
+        /// items array
+        /// </summary>
+        /// <value>items array</value>
+        [JsonProperty("items")]
+        public List<PostCharactersCharacterIdFittingsItem> Items { get; set; }
+    }
+
+    public class FittingItem : ModelBase<FittingItem>
+    {
+        /// <summary>
+        /// type_id integer
+        /// </summary>
+        /// <value>type_id integer</value>
+        [JsonProperty("type_id")] public int? TypeId { get; set; }
+
+        /// <summary>
+        /// flag integer
+        /// </summary>
+        /// <value>flag integer</value>
+        [JsonProperty("flag")] public int? Flag { get; set; }
+
+        /// <summary>
+        /// quantity integer
+        /// </summary>
+        /// <value>quantity integer</value>
+        [JsonProperty("quantity")] public int? Quantity { get; set; }
+    }
+}
