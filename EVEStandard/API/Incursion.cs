8180fa2d2fea5ca1f0df7ef8619917bfe9f3655b
Gehnster <gehnster@withineveonline>
2018-04-14 21:10:24 -0600

    Incursion end-point

--- EVEStandard/API/Incursion.cs
+++ EVEStandard/API/Incursion.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
+﻿using EVEStandard.Enumerations;
+using EVEStandard.Models.API;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EVEStandard.API
 {
+    using Models;
+
     public class Incursion : APIBase
     {
         private ILogger Logger { get; } = LibraryLogging.CreateLogger<Incursion>();
         internal Incursion(string dataSource) : base(dataSource)
         {
+        }
+
+        public async Task<List<Incursion>> ListIncursionsV1Async()
+        {
+            var responseModel = await this.GetAsync("/v1/incursions/");
+
+            checkResponse("ListIncursionsV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<List<Incursion>>(responseModel.JSONString);
         }
     }
 }
