51eef651ca889960c13539b997c103a352e2596c
Gehnster <gehnster@withineveonline>
2018-04-16 09:36:54 -0600

    Insurance end-point

--- EVEStandard/API/Insurance.cs
+++ EVEStandard/API/Insurance.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using EVEStandard.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EVEStandard.API
 {
@@ -10,6 +13,15 @@ namespace EVEStandard.API
         private ILogger Logger { get; } = LibraryLogging.CreateLogger<Insurance>();
         internal Insurance(string dataSource) : base(dataSource)
         {
+        }
+
+        public async Task<List<InsurancePrice>> ListInsuranceLevelsV1Async()
+        {
+            var responseModel = await this.GetAsync("/v1/insurance/prices/");
+
+            checkResponse("ListInsuranceLevelsV1Async", responseModel.Error, responseModel.LegacyWarning, this.Logger);
+
+            return JsonConvert.DeserializeObject<List<InsurancePrice>>(responseModel.JSONString);
         }
     }
 }
