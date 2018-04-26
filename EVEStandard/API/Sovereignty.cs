a736ef20d5805517c75a96959efcb546d45a1a1a
Gehnster <gehnster@withineveonline>
2018-04-25 23:23:11 -0600

    Sov end-points

--- EVEStandard/API/Sovereignty.cs
+++ EVEStandard/API/Sovereignty.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EVEStandard.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 
 namespace EVEStandard.API
@@ -8,6 +12,33 @@ namespace EVEStandard.API
         private ILogger Logger { get; } = LibraryLogging.CreateLogger<Sovereignty>();
         internal Sovereignty(string dataSource) : base(dataSource)
         {
+        }
+
+        public async Task<List<SovereigntyStructure>> ListSovereigntyStructuresV1Async()
+        {
+            var responseModel = await GetAsync("/v1/sovereignty/structures/");
+
+            checkResponse("ListSovereigntyStructuresV1Async", responseModel.Error, responseModel.LegacyWarning, Logger);
+
+            return JsonConvert.DeserializeObject<List<SovereigntyStructure>>(responseModel.JSONString);
+        }
+
+        public async Task<List<SovereigntyCampaign>> ListSovereigntyCampaignsV1Async()
+        {
+            var responseModel = await GetAsync("/v1/sovereignty/campaigns/");
+
+            checkResponse("ListSovereigntyCampaignsV1Async", responseModel.Error, responseModel.LegacyWarning, Logger);
+
+            return JsonConvert.DeserializeObject<List<SovereigntyCampaign>>(responseModel.JSONString);
+        }
+
+        public async Task<List<SovereigntyMap>> ListSovereigntyOfSystemsV1Async()
+        {
+            var responseModel = await GetAsync("/v1/sovereignty/map/");
+
+            checkResponse("ListSovereigntyOfSystemsV1Async", responseModel.Error, responseModel.LegacyWarning, Logger);
+
+            return JsonConvert.DeserializeObject<List<SovereigntyMap>>(responseModel.JSONString);
         }
     }
 }
---
+++ EVEStandard/Models/SovereigntyMap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace EVEStandard.Models
+{
+    public class SovereigntyMap : ModelBase<SovereigntyMap>
+    {
+        /// <summary>
+        /// system_id integer
+        /// </summary>
+        /// <value>system_id integer</value>
+        [JsonProperty("system_id")]
+        public int? SystemId { get; set; }
+
+        /// <summary>
+        /// alliance_id integer
+        /// </summary>
+        /// <value>alliance_id integer</value>
+        [JsonProperty("alliance_id")]
+        public int? AllianceId { get; set; }
+
+        /// <summary>
+        /// corporation_id integer
+        /// </summary>
+        /// <value>corporation_id integer</value>
+        [JsonProperty("corporation_id")]
+        public int? CorporationId { get; set; }
+
+        /// <summary>
+        /// faction_id integer
+        /// </summary>
+        /// <value>faction_id integer</value>
+        [JsonProperty("faction_id")]
+        public int? FactionId { get; set; }
+    }
+}
