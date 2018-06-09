2eeff055a32a070ef8faf9ddd5918d25b3621de7
Lucas Styles <lucas.styles@hfma.org.uk>
2018-06-09 21:41:44 +0100

    Incursion API now returns Incursion model instead of Incursion API class

--- EVEStandard/API/Incursion.cs
+++ EVEStandard/API/Incursion.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using EVEStandard.Models.API;
+﻿using EVEStandard.Models.API;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EVEStandard.API
 {
@@ -10,17 +9,18 @@ namespace EVEStandard.API
     public class Incursion : APIBase
     {
         private ILogger Logger { get; } = LibraryLogging.CreateLogger<Incursion>();
+
         internal Incursion(string dataSource) : base(dataSource)
         {
         }
 
-        public async Task<ESIModelDTO<List<Incursion>>> ListIncursionsV1Async(string ifNoneMatch = null)
+        public async Task<ESIModelDTO<List<Models.Incursion>>> ListIncursionsV1Async(string ifNoneMatch = null)
         {
             var responseModel = await GetAsync("/v1/incursions/", ifNoneMatch);
 
             checkResponse("ListIncursionsV1Async", responseModel.Error, responseModel.Message, responseModel.LegacyWarning, Logger);
 
-            return returnModelDTO<List<Incursion>>(responseModel);
+            return returnModelDTO<List<Models.Incursion>>(responseModel);
         }
     }
 }
