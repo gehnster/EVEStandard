f7e0d830f6b0f8f66fa37f00746c84bf45810f2d
Lucas Styles <lucas.styles@outlook.com>
2018-06-12 00:19:44 +0100

    Fittings  API

--- EVEStandard/API/Fittings.cs
+++ EVEStandard/API/Fittings.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 
 namespace EVEStandard.API
 {
+    /// <summary>
+    /// Fittings API
+    /// </summary>
+    /// <seealso cref="EVEStandard.API.APIBase" />
     public class Fittings : APIBase
     {
         private readonly ILogger logger = LibraryLogging.CreateLogger<Fittings>();
@@ -15,33 +19,54 @@ namespace EVEStandard.API
         {
         }
 
+        /// <summary>
+        /// Delete a fitting from a character.
+        /// <para>DELETE /characters/{character_id}/fittings/{fitting_id}/</para>
+        /// </summary>
+        /// <param name="auth">The <see cref="AuthDTO"/> object.</param>
+        /// <param name="fittingId">ID for a fitting of this character.</param>
+        /// <returns></returns>
         public async Task DeleteFittingV1Async(AuthDTO auth, long fittingId)
         {
             checkAuth(auth, Scopes.ESI_FITTINGS_WRITE_FITTINGS_1);
 
-            var responseModel = await DeleteAsync("/v1/characters/" + auth.CharacterId + "/fittings/" + fittingId + "/", auth);
+            var responseModel = await DeleteAsync($"/v1/characters/{auth.CharacterId}/fittings/{fittingId}/", auth);
 
             checkResponse("DeleteFittingV1Async", responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
         }
 
+        /// <summary>
+        /// Return fittings of a character.
+        /// <para>GET /characters/{character_id}/fittings/</para>
+        /// </summary>
+        /// <param name="auth">The <see cref="AuthDTO"/> object.</param>
+        /// <param name="ifNoneMatch">ETag from a previous request. A 304 will be returned if this matches the current ETag.</param>
+        /// <returns><see cref="ESIModelDTO{T}"/> containing a list fittings.</returns>
         public async Task<ESIModelDTO<List<CharacterFitting>>> GetFittingsV1Async(AuthDTO auth, string ifNoneMatch = null)
         {
             checkAuth(auth, Scopes.ESI_FITTINGS_READ_FITTINGS_1);
 
-            var responseModel = await GetAsync("/v1/characters/" + auth.CharacterId + "/fittings/", auth, ifNoneMatch);
+            var responseModel = await GetAsync($"/v1/characters/{auth.CharacterId}/fittings/", auth, ifNoneMatch);
 
-            checkResponse("TrackCorporationMembersV1Async", responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
+            checkResponse(nameof(GetFittingsV1Async), responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
 
             return returnModelDTO<List<CharacterFitting>>(responseModel);
         }
 
+        /// <summary>
+        /// Save a new fitting for a character.
+        /// <para>POST /characters/{character_id}/fittings/</para>
+        /// </summary>
+        /// <param name="auth">The <see cref="AuthDTO"/> object.</param>
+        /// <param name="fitting">Details about the new fitting.</param>
+        /// <returns><see cref="ESIModelDTO{T}"/> containing a list fittings.</returns>
         public async Task<ESIModelDTO<long>> CreateFittingV1Async(AuthDTO auth, ShipFitting fitting)
         {
             checkAuth(auth, Scopes.ESI_FITTINGS_READ_FITTINGS_1);
 
-            var responseModel = await PostAsync("/v1/characters/" + auth.CharacterId + "/fittings/", auth, fitting);
+            var responseModel = await PostAsync($"/v1/characters/{auth.CharacterId}/fittings/", auth, fitting);
 
-            checkResponse("TrackCorporationMembersV1Async", responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
+            checkResponse(nameof(CreateFittingV1Async), responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
 
             return returnModelDTO<long>(responseModel);
         }
