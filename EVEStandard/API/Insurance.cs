087a51160b183d5d2259cedb0eff4dc18d4f5555
Lucas Styles <lucas.styles@outlook.com>
2018-06-12 22:50:45 +0100

    Missing ifNoneMatch comment

--- EVEStandard/API/Insurance.cs
+++ EVEStandard/API/Insurance.cs
@@ -22,7 +22,7 @@ namespace EVEStandard.API
         /// Return available insurance levels for all ship types.
         /// <para>GET /insurance/prices/</para>
         /// </summary>
-        /// <param name="ifNoneMatch">If none match.</param>
+        /// <param name="ifNoneMatch">ETag from a previous request. A 304 will be returned if this matches the current ETag.</param>
         /// <returns><see cref="ESIModelDTO{T}"/> containing a list of insurance levels for all ship types.</returns>
         public async Task<ESIModelDTO<List<InsurancePrice>>> ListInsuranceLevelsV1Async(string ifNoneMatch = null)
         {
