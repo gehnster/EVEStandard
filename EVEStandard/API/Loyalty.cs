de583093f7d26a95d5d62889ba892a83f10dfe8c
Lucas Styles <lucas.styles@outlook.com>
2018-06-12 22:50:57 +0100

    Loyalty API

--- EVEStandard/API/Loyalty.cs
+++ EVEStandard/API/Loyalty.cs
@@ -7,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace EVEStandard.API
 {
-
+    /// <summary>
+    /// Loyalty API
+    /// </summary>
+    /// <seealso cref="EVEStandard.API.APIBase" />
     public class Loyalty : APIBase
     {
         private readonly ILogger logger = LibraryLogging.CreateLogger<Loyalty>();
@@ -16,22 +19,35 @@ namespace EVEStandard.API
         {
         }
 
+        /// <summary>
+        /// Return a list of offers from a specific corporation’s loyalty store.
+        /// </summary>
+        /// <param name="corporationId">An EVE corporation ID.</param>
+        /// <param name="ifNoneMatch">ETag from a previous request. A 304 will be returned if this matches the current ETag.</param>
+        /// <returns><see cref="ESIModelDTO{T}"/> containing a list of offers.</returns>
         public async Task<ESIModelDTO<List<LoyaltyStoreOffer>>> ListLoyaltyStoreOffersV1Async(int corporationId, string ifNoneMatch = null)
         {
-            var responseModel = await GetAsync("/v1/loyalty/stores/" + corporationId + "/offers/", ifNoneMatch);
+            var responseModel = await GetAsync($"/v1/loyalty/stores/{corporationId}/offers/", ifNoneMatch);
 
-            checkResponse("ListLoyaltyStoreOffersV1Async", responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
+            checkResponse(nameof(ListLoyaltyStoreOffersV1Async), responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
 
             return returnModelDTO<List<LoyaltyStoreOffer>>(responseModel);
         }
 
+        /// <summary>
+        /// Return a list of loyalty points for all corporations the character has worked for.
+        /// <para>GET /characters/{character_id}/loyalty/points/</para>
+        /// </summary>
+        /// <param name="auth">The <see cref="AuthDTO"/> object.</param>
+        /// <param name="ifNoneMatch">ETag from a previous request. A 304 will be returned if this matches the current ETag.</param>
+        /// <returns><see cref="ESIModelDTO{T}"/> containing a list of loyalty points.</returns>
         public async Task<ESIModelDTO<List<LoyaltyPoints>>> GetLoyaltyPointsV1Async(AuthDTO auth, string ifNoneMatch = null)
         {
             checkAuth(auth, Scopes.ESI_CHARACTERS_READ_LOYALTY_1);
 
-            var responseModel = await GetAsync("/v1/characters/" + auth.CharacterId + "/loyalty/points/", auth, ifNoneMatch);
+            var responseModel = await GetAsync($"/v1/characters/{auth.CharacterId}/loyalty/points/", auth, ifNoneMatch);
 
-            checkResponse("GetLoyaltyPointsV1Async", responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
+            checkResponse(nameof(GetLoyaltyPointsV1Async), responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
 
             return returnModelDTO<List<LoyaltyPoints>>(responseModel);
         }
