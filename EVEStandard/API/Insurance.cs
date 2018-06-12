5d730f80f7b07ae3460c1578cf510f5e9722aa5a
Lucas Styles <lucas.styles@outlook.com>
2018-06-12 19:24:08 +0100

    Insurance API

--- EVEStandard/API/Insurance.cs
+++ EVEStandard/API/Insurance.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace EVEStandard.API
 {
+    /// <summary>
+    /// Insurance API
+    /// </summary>
+    /// <seealso cref="EVEStandard.API.APIBase" />
     public class Insurance : APIBase
     {
         private readonly ILogger logger = LibraryLogging.CreateLogger<Insurance>();
@@ -14,11 +18,17 @@ namespace EVEStandard.API
         {
         }
 
+        /// <summary>
+        /// Return available insurance levels for all ship types.
+        /// <para>GET /insurance/prices/</para>
+        /// </summary>
+        /// <param name="ifNoneMatch">If none match.</param>
+        /// <returns></returns>
         public async Task<ESIModelDTO<List<InsurancePrice>>> ListInsuranceLevelsV1Async(string ifNoneMatch = null)
         {
             var responseModel = await GetAsync("/v1/insurance/prices/", ifNoneMatch);
 
-            checkResponse("ListInsuranceLevelsV1Async", responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
+            checkResponse(nameof(ListInsuranceLevelsV1Async), responseModel.Error, responseModel.Message, responseModel.LegacyWarning, logger);
 
             return returnModelDTO<List<InsurancePrice>>(responseModel);
         }
