b95a6c528f56787a0b514d3100af6744339b0c6d
Richard Mountain <web@richardmountain.me>
2022-10-25 16:16:04 +0100

    Added the missing 'item' location type enum value.

--- EVEStandard/Enumerations/LocationTypeEnum.cs
+++ EVEStandard/Enumerations/LocationTypeEnum.cs
@@ -5,6 +5,7 @@ namespace EVEStandard.Enumerations
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum LocationTypeEnum
     {
+        item,
         station,
         solar_system,
         other
