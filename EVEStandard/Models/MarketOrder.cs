364dbd24e22546410fea75861afbb4503829eee5
Fluffy Neko <244721220+fluffy-the-cat@users.noreply.github.com>
2026-07-22 09:22:17 +0200

    feat: convert MarketOrder's Range using custom converter

--- EVEStandard/Models/MarketOrder.cs
+++ EVEStandard/Models/MarketOrder.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace EVEStandard.Models
@@ -61,6 +63,7 @@ namespace EVEStandard.Models
         /// </summary>
         /// <value>range string</value>
         [JsonPropertyName("range")]
+        [JsonConverter(typeof(EveRangeStringConverter))]
         public string Range { get; set; }
 
         /// <summary>
@@ -94,5 +97,59 @@ namespace EVEStandard.Models
         public long VolumeTotal { get; set; }
 
         #endregion Properties
+
+        #region JSON Converters
+
+        /**
+         * Converts the range strings using Spans.
+         * <br/>
+         * Why? It is about 10% faster compared to the base method, and as string literals are interned by default, it
+         * also reduces the memory usage of the MarketOrder objects in memory.
+         */
+        public class EveRangeStringConverter : JsonConverter<string>
+        {
+            // Note: ValueTextEquals accepts a ReadonlySpan<T> as parameter, strings are not directly convertable to this,
+            // as a result, we need to convert them manually down to byte[] which will implicitly convert to Span during
+            // use.
+            // Currently, the project uses C#8, but with C#11 we could remove these and use u8 literals in the Read function,
+            // such as <code>if (reader.ValueTextEquals("station"u8)) return "station";</code>.
+            private static readonly byte[] StationUtf8 = Encoding.UTF8.GetBytes("station");
+            private static readonly byte[] RegionUtf8 = Encoding.UTF8.GetBytes("region");
+            private static readonly byte[] SolarSystemUtf8 = Encoding.UTF8.GetBytes("solarsystem");
+            private static readonly byte[] Range1Utf8 = Encoding.UTF8.GetBytes("1");
+            private static readonly byte[] Range2Utf8 = Encoding.UTF8.GetBytes("2");
+            private static readonly byte[] Range3Utf8 = Encoding.UTF8.GetBytes("3");
+            private static readonly byte[] Range4Utf8 = Encoding.UTF8.GetBytes("4");
+            private static readonly byte[] Range5Utf8 = Encoding.UTF8.GetBytes("5");
+            private static readonly byte[] Range10Utf8 = Encoding.UTF8.GetBytes("10");
+            private static readonly byte[] Range20Utf8 = Encoding.UTF8.GetBytes("20");
+            private static readonly byte[] Range30Utf8 = Encoding.UTF8.GetBytes("30");
+            private static readonly byte[] Range40Utf8 = Encoding.UTF8.GetBytes("40");
+
+            public override string Read(ref Utf8JsonReader reader, global::System.Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (reader.ValueTextEquals(StationUtf8)) return "station";
+                if (reader.ValueTextEquals(RegionUtf8)) return "region";
+                if (reader.ValueTextEquals(SolarSystemUtf8)) return "solarsystem";
+                if (reader.ValueTextEquals(Range1Utf8)) return "1";
+                if (reader.ValueTextEquals(Range2Utf8)) return "2";
+                if (reader.ValueTextEquals(Range3Utf8)) return "3";
+                if (reader.ValueTextEquals(Range4Utf8)) return "4";
+                if (reader.ValueTextEquals(Range5Utf8)) return "5";
+                if (reader.ValueTextEquals(Range10Utf8)) return "10";
+                if (reader.ValueTextEquals(Range20Utf8)) return "20";
+                if (reader.ValueTextEquals(Range30Utf8)) return "30";
+                if (reader.ValueTextEquals(Range40Utf8)) return "40";
+
+                return reader.GetString();
+            }
+
+            public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+            {
+                writer.WriteStringValue(value);
+            }
+        }
+
+        #endregion
     }
 }
